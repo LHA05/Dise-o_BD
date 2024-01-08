493df79005683bed01e63b3b049024ecc3752556
Your Name <you@example.com>
2024-01-08 13:28:10 -0600

    Diseño de la base datos

---
+++ js/alerts.js
@@ -0,0 +1,28 @@
+function correcto() {
+        swal("Conexion Exitosa..!", " ", "success");
+}
+
+function listo() {
+        swal("Correccion Lista", " ", "success");
+}
+
+function eliminar() {
+        swal("Eliminado!", " ", "error");    
+}
+
+function editar() {
+        swal("Editado", " ", "warning");
+
+}
+function insert() {
+        swal("Insert", " ", "success");
+}
+
+function drop() {
+        swal("Drop!", " ", "error");    
+}
+
+function select() {
+        swal("Select", " ", "warning");
+
+}
